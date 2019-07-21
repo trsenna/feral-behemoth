52fa7863c1f193f0f20021b9eb5134b914bcf55c
Thiago Senna <trsenna@gmail.com>
2019-07-21 00:37:45 -0300

    big cleanup, keep simple

--- app-functionality/app-functionality.php
+++ app-functionality/app-functionality.php
@@ -10,18 +10,3 @@
 
 define( 'APP_FUNCTIONALITY_PLUGIN', true );
 define( 'APP_FUNCTIONALITY_PLUGIN_FILE', __FILE__ );
-
-
-# ------------------------------------------------------------------------------
-# Bootstrap the plugin.
-# ------------------------------------------------------------------------------
-#
-# Maybe load the bootstrap files. Note that autoloading should happen
-# first so that any classes/functions are available that we might need.
-#
-
-add_action( 'app/foundation/compat/safe', function () {
-
-    require_once( __DIR__ . '/includes/bootstrap-autoload.php' );
-
-} );
