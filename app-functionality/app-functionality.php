d32037754edead8b70af6806f0f568cb92bf58e4
Thiago Senna <trsenna@gmail.com>
2021-03-27 22:09:45 -0300

    funcionality plugin maybe done

--- app-functionality/app-functionality.php
+++ app-functionality/app-functionality.php
@@ -1,30 +1,46 @@
 <?php
-/**
- * Plugin Name: App Functionality
- * Author:      Thiago Senna
- * Author URI:  http://thremes.com.br
- * Description: The application functionality plugin.
- * License:     GNU General Public License v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- *
- * @package   App\Functionality
- */
 
-define( 'APP_FUNCTIONALITY_PLUGIN', true );
-define( 'APP_FUNCTIONALITY_PLUGIN_FILE', __FILE__ );
+/* ---------------------------------------------------------------------------------------------
 
+	Plugin Name:    App Functionality
+	Description:    The application functionality plugin.
+	Author:         Thiago Senna
+	Author URI:     https://thremes.com.br
+	License:        GNU General Public License version 2.0
+	License URI:    http://www.gnu.org/licenses/gpl-2.0.html
+	Version:        0.1.0
+	Requires PHP:   7.4
+
+	This plugin, like WordPress, is licensed under the GPL.
+	Use it to make something cool, have fun, and share what you've learned with others.
+
+--------------------------------------------------------------------------------------------- */
+
+use Dalen\Plugin;
+
+define('APP_FUNCTIONALITY_PLUGIN', true);
+define('APP_FUNCTIONALITY_PLUGIN_FILE', __FILE__);
 
 # ------------------------------------------------------------------------------
-# Bootstrap the plugin.
+# Lorem ipsum dolor sit amet
 # ------------------------------------------------------------------------------
 #
-# Maybe load the bootstrap files. Note that autoloading should happen
-# first so that any classes/functions are available that we might need.
+# Suspendisse sodales ipsum non justo imperdiet, ut lacinia erat
+# cursus. Vestibulum dictum nisi ligula, in dictum justo pulvinar quis.
 #
 
-add_action( 'dalen/bootstrap/plugins', function () {
+add_action('mazoo/bootstrap', function (Plugin $plugin) {
+	// ...
+});
 
-    require_once( __DIR__ . '/includes/bootstrap-autoload.php' );
-    require_once( __DIR__ . '/includes/bootstrap-plugin.php' );
+# ------------------------------------------------------------------------------
+# Lorem ipsum dolor sit amet
+# ------------------------------------------------------------------------------
+#
+# Suspendisse sodales ipsum non justo imperdiet, ut lacinia erat
+# cursus. Vestibulum dictum nisi ligula, in dictum justo pulvinar quis.
+#
 
-} );
+add_filter('mazoo/autoload/files', function (array $files) {
+	return $files;
+});
--- app-theme/functions.php
+++ app-theme/functions.php
@@ -16,3 +16,15 @@ define('APP_THEME_FILE', __FILE__);
 add_action('mezu/bootstrap', function (Theme $theme) {
     // ...
 });
+
+# ------------------------------------------------------------------------------
+# Lorem ipsum dolor sit amet
+# ------------------------------------------------------------------------------
+#
+# Suspendisse sodales ipsum non justo imperdiet, ut lacinia erat
+# cursus. Vestibulum dictum nisi ligula, in dictum justo pulvinar quis.
+#
+
+add_filter('mezu/autoload/files', function (array $files) {
+    return $files;
+});
